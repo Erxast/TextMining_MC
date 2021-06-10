a15a46b6d7d6e5fc7ebb4e8020258fe5e3079297
hugues.escoffier <hugues.escoffier@gmail.com>
2021-06-10 17:43:24 +0200

    Api   > queryK   > web_env

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def api():
     rob = requests.get(
         'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=congenital+myopathy&retmode=json&usehistory=y')
     #  print(rob.status_code)
-    all_json = rob.json()
+    #  all_json = rob.json()
     query_key = rob.json()['esearchresult']['querykey']
     web_env = rob.json()['esearchresult']['webenv']
     print('query_key=', query_key)
